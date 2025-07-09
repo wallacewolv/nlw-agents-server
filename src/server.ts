49028e5fa9b79d5ff805da92f72c960114514972
Wallace W <wallace_wolv@hotmail.com>
2025-07-09 17:52:07 -0300

    feat: Implementação da rota para obter informações das salas e .env.example

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,8 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import { sql } from './db/connection.ts'
 import { env } from './env.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -20,5 +20,7 @@ app.setValidatorCompiler(validatorCompiler)
 app.get('/health', () => {
   return { status: 'ok' }
 })
+
+app.register(getRoomsRoute)
 
 app.listen({ port: env.PORT })
