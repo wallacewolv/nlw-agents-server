8912737278535a7f4aeaf2275f62dfa81b3b3550
Wallace W <wallace_wolv@hotmail.com>
2025-07-09 20:28:44 -0300

    feat: Implementando rota para criação de salas com validação de dados

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -22,5 +23,6 @@ app.get('/health', () => {
 })
 
 app.register(getRoomsRoute)
+app.register(createRoomRoute)
 
 app.listen({ port: env.PORT })
