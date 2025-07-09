c963261122fb6c8df9c159491772d904e769cbae
Wallace W <wallace_wolv@hotmail.com>
2025-07-09 13:21:45 -0300

    chore: Configurando biome

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,23 @@
-import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
-} from "fastify-type-provider-zod"
-import { fastifyCors } from "@fastify/cors";
-import { env } from "./env.ts";
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
 })
 
-app.setSerializerCompiler(serializerCompiler);
-app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
 app.get('/health', () => {
-  return { status: 'ok' };
+  return { status: 'ok' }
 })
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log('HTTP server running!')
-})
+app.listen({ port: env.PORT })
