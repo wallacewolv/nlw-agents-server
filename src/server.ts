e76e134a54381ca35333104ccd37a35005b78f71
Wallace W <wallace_wolv@hotmail.com>
2025-07-09 20:44:08 -0300

    feat: Adicionando rota para obter perguntas de uma sala

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import {
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomsQuestionsRoute } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -24,5 +25,6 @@ app.get('/health', () => {
 
 app.register(getRoomsRoute)
 app.register(createRoomRoute)
+app.register(getRoomsQuestionsRoute)
 
 app.listen({ port: env.PORT })
